7b4e0f190ee778a57c6cf2f7fd5db5bd8a5e4518
bherbin <33049464+bherbin@users.noreply.github.com>
2017-11-10 04:52:03 -0500

    update app

--- index_htm_files/12.js
+++ index_htm_files/12.js
@@ -1,4 +1,4 @@
-window.local_mosaic_slide_146_data = [
+window.local_mosaic_slide_142_data = [
 {
  direction: "0",
  speed: "0.5",
@@ -6,8 +6,8 @@ window.local_mosaic_slide_146_data = [
 },
 {
  heading: "index_htm_files/2354.png",
- image: "index_htm_files/2375.jpg",
+ image: "index_htm_files/2355.jpg",
  link: "#",
- text: "index_htm_files/2376.png"
+ text: "index_htm_files/2356.png"
 }
 ];
--- index_htm_files/9.js
+++ index_htm_files/9.js
@@ -1,4 +1,4 @@
-window.local_mosaic_slide_143_data = [
+window.local_mosaic_slide_145_data = [
 {
  direction: "0",
  speed: "0.5",
@@ -6,8 +6,8 @@ window.local_mosaic_slide_143_data = [
 },
 {
  heading: "index_htm_files/2354.png",
- image: "index_htm_files/2360.jpg",
+ image: "index_htm_files/2370.jpg",
  link: "#",
- text: "index_htm_files/2361.png"
+ text: "index_htm_files/2371.png"
 }
 ];
